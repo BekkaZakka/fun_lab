da344f45b2223bc1c89123d59dea07b42d1cd9b7
Abylai003 <86314820+Abylai003@users.noreply.github.com>
2023-04-22 16:16:31 +0600

    11 lab new code 2 example

--- 11_2.py
+++ 11_2.py
@@ -1,7 +1,28 @@
-from tkinter import *
+# import canvas as canvas
+import tkinter as tk
 
-root = Tk()
-canvas = Canvas(root, width=300, height=300)
-canvas.create_text(150, 150, text="Abylai", font=("Arial", 30))
+# Создаем окно
+window = tk.Tk()
+
+# Устанавливаем заголовок окна
+window.title("Логотип")
+
+# Создаем холст
+canvas = tk.Canvas(window, width=300, height=300)
+
+# Изменяем цвет заднего фона
+canvas.configure(bg="white")
+
+# canvas.create_line(0, 0, 300, 300, width=3, fill="purple")
+canvas.create_rectangle(50, 50, 250, 250, fill="white", outline="black", width=2)
+canvas.create_oval(100, 100, 200, 200, fill="red")
+# Создаем текстовый объект с именем
+text = canvas.create_text(150, 150, text="Abylaikhan Kosherbaev",
+                          font=("Arial", 20), fill="white")
+canvas.create_text(152, 152, text="Abylaikhan Kosherbaev",
+                   font=("Arial", 20), fill="black")
+# Отображаем холст
 canvas.pack()
-root.mainloop()
+
+# Запускаем цикл событий
+window.mainloop()
